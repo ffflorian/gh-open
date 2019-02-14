ac866f469fb99bc255d4004d0226d191268a1662
Florian Keller <github@floriankeller.de>
2019-02-14 14:40:19 +0100

    feat: Use commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,72 +1,23 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
 import * as path from 'path';
-
 import * as findUp from 'find-up';
+import * as program from 'commander';
 import opn = require('opn');
 
 import {getFullUrl} from './gh-open';
 
-const SCRIPT_NAME = 'gh-open';
-const args = process.argv;
+const {name, version, description}: {name: string; version: string; description: string} = require('../package.json');
 
-const options = {
-  baseDir: './',
-  printOnly: false,
-};
+program
+  .name(name.replace(/^@[^/]+\//, ''))
+  .description(description)
+  .option('-p, --print', 'Just print the URL', false)
+  .arguments('[directory]',)
+  .version(version, '-v, --version')
+  .parse(process.argv);
 
-function displayHelp(): never {
-  const usageText = `Usage: ${SCRIPT_NAME} [-h] [switch] [directory]
-
-Commands:
- --help (-h)   Show help text
-
-Switches:
- --print (-p)  Just print the URL
-
-Example: ${SCRIPT_NAME} -p git_project/`;
-
-  console.info(usageText);
-  return process.exit();
-}
-
-for (let argIndex = 2; argIndex < args.length; argIndex++) {
-  const arg = args[argIndex];
-
-  switch (arg) {
-    case '-h':
-    case '--help': {
-      displayHelp();
-      break;
-    }
-    case '-p':
-    case '--print-only': {
-      options.printOnly = true;
-      continue;
-    }
-    default: {
-      if (arg.startsWith('-')) {
-        let isDirectory = false;
-        try {
-          isDirectory = fs.statSync(arg).isDirectory();
-        } catch (error) {}
-
-        if (!isDirectory) {
-          console.error(`Invalid argument "${arg}".\n`);
-          displayHelp();
-        } else {
-          options.baseDir = arg;
-        }
-      } else {
-        options.baseDir = arg;
-      }
-      break;
-    }
-  }
-}
-
-const resolvedBaseDir = path.resolve(options.baseDir);
+const resolvedBaseDir = path.resolve(program.args[0] || '.');
 
 findUp('.git', {cwd: resolvedBaseDir})
   .then(gitDir => {
@@ -76,7 +27,7 @@ findUp('.git', {cwd: resolvedBaseDir})
     return getFullUrl(gitDir);
   })
   .then(fullUrl => {
-    if (options.printOnly) {
+    if (program.print) {
       console.info(fullUrl);
       return;
     }
