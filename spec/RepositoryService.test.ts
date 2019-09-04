93eede47aaadac8a2512e1b3a9635a47114078c8
Florian Keller <ffflorian@users.noreply.github.com>
2019-09-04 14:51:40 +0200

    feat: Use class for RepositoryService (BREAKING) (#176)
    BREAKING CHANGES: Repository Service is now a class.

--- spec/RepositoryService.test.ts
+++ spec/RepositoryService.test.ts
@@ -1,13 +1,15 @@
-import {parser} from '../src/RepositoryService';
+import {RepositoryService} from '../src/RepositoryService';
 
 describe('RepositoryService', () => {
+  const repositoryService = new RepositoryService();
+
   describe('getFullUrl', () => {
     const normalizedUrl = 'https://github.com/ffflorian/gh-open';
 
     const testRegex = (str: string) => {
-      const match = parser.fullUrl.exec(str);
+      const match = repositoryService['parser'].fullUrl.exec(str);
       expect(match![0]).toEqual(jasmine.any(String));
-      const replaced = str.replace(parser.fullUrl, 'https://$1/$2');
+      const replaced = str.replace(repositoryService['parser'].fullUrl, 'https://$1/$2');
       expect(replaced).toBe(normalizedUrl);
     };
 
@@ -51,7 +53,7 @@ describe('RepositoryService', () => {
     const rawUrl = 'git@github.com:ffflorian/gh-open.git';
 
     const testRegex = (str: string) => {
-      const match = parser.rawUrl.exec(str);
+      const match = repositoryService['parser'].rawUrl.exec(str);
       expect(match!.groups!.rawUrl).toBe(rawUrl);
     };
 
@@ -67,7 +69,7 @@ describe('RepositoryService', () => {
 
     describe('parseGitBranch', () => {
       const testRegex = (str: string, result: string) => {
-        const match = parser.gitBranch.exec(str);
+        const match = repositoryService['parser'].gitBranch.exec(str);
         expect(match!.groups!.branch).toBe(result);
       };
 
